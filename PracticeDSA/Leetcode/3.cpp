d25bffeb464205d1da1cfa39bde4a936144365fc
Tushar Kumar <kumartushar78867@gmail.com>
2024-12-22 03:15:10 +0530

    data-structures-and-algorithms

---
+++ PracticeDSA/Leetcode/3.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+// using namespace std;
+// #include <vector>
+
+// int main()
+// {
+//     vector<int> nums = {-1,-4,-1,4};
+//     int count = 0;
+//     int size = nums.size();
+
+//     for (int i = 0; i < size - 2; ++i)
+//     {
+//         if (nums[i] + nums[i + 2] == nums[i + 1] / 2.0)
+//         {
+        
+//             ++count;
+//         }
+//     }
+//     // cout << count;
+// }
+
+// Revision  
