587fbf4c5a40853cb6ec22304b4c1ffc49ca6a4e
Tushar Kumar <kumartushar78867@gmail.com>
2025-01-20 23:03:54 +0530

    data-structures-and-algorithms

--- PracticeDSA/gfg/16_01_25_gfg.cpp
+++ PracticeDSA/gfg/16_01_25_gfg.cpp
@@ -1,86 +1,120 @@
-// //  we need to implement the linked list from scratch and also need to reverse it.
+//  we need to implement the linked list from scratch and also need to reverse it.
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// class LinkedList
-// {
+class LinkedList
+{
 
-//     // we need to create the singly linked list.
-// public:
-//     int data;
-//     LinkedList *next;
-//     LinkedList(int data)
-//     {
-//         this->data = data;
-//         this->next = nullptr;
-//     }
-// };
+    // we need to create the singly linked list.
+public:
+    int data;
+    LinkedList *next;
+    LinkedList(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
 
-// void print(LinkedList *&head)
-// {
+void print(LinkedList *&head)
+{
 
-//     LinkedList *temp = head;
+    LinkedList *temp = head;
 
-//     while (temp != nullptr)
-//     {
-//         cout << temp->data << " -> ";
-//         temp = temp->next;
-//     }
-//     cout << "nullptr" << endl;
-// }
+    while (temp != nullptr)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "nullptr" << endl;
+}
 
-// void insertAtHead(LinkedList *&head, LinkedList *&tail, int data)
-// {
-//     LinkedList *newNode = new LinkedList(data);
-//     //  initially the head is null.
-//     if (head == nullptr)
-//     {
-//         //  means that we dont have any kind of linked list so we need to update the head and the tail pointer.
-//         head = newNode;
-//         tail = newNode;
-//         return;
-//     }
-//     else
-//     {
-//         //  here we have the linked list if it may be the on or more than one node..
-//         newNode->next = head;
-//         head = newNode;
-//     }
-// }
+void insertAtHead(LinkedList *&head, LinkedList *&tail, int data)
+{
+    LinkedList *newNode = new LinkedList(data);
+    //  initially the head is null.
+    if (head == nullptr)
+    {
+        //  means that we dont have any kind of linked list so we need to update the head and the tail pointer.
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    else
+    {
+        //  here we have the linked list if it may be the on or more than one node..
+        newNode->next = head;
+        head = newNode;
+    }
+}
 
-// LinkedList *reverseLinkedList(LinkedList *&head)
-// {
-//     LinkedList *prev = nullptr;
-//     LinkedList *next = nullptr;
+void insertAtTail(LinkedList *&head, LinkedList *&tail, int data)
+{
+    LinkedList *newNode = new LinkedList(data);
+    //  initially the head is null.
+    if (tail == nullptr)
+    {
+        //  means that we dont have any kind of linked list so we need to update the head and the tail pointer.
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    else
+    {
+        //  here we have the linked list if it may be the on or more than one node..
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
 
-//     while (head)
-//     {
-//         next = head->next;
-//         head->next = prev;
-//         prev = head;
-//         head = next;
-//     }
-//     return prev;
-// }
-// int main()
-// {
+LinkedList *reverseLinkedList(LinkedList *&head)
+{
+    LinkedList *prev = nullptr;
+    LinkedList *next = nullptr;
 
-//     LinkedList *head = nullptr;
-//     LinkedList *tail = nullptr;
+    while (head)
+    {
+        next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
+int main()
+{
 
-//     //  craete the linked list and add the nodes on the head;
-//     insertAtHead(head, tail, 10);
-//     print(head);
-//     insertAtHead(head, tail, 20);
-//     print(head);
-//     insertAtHead(head, tail, 30);
-//     print(head);
-//     insertAtHead(head, tail, 40);
-//     print(head);
-//     insertAtHead(head, tail, 50);
-//     print(head);
+    LinkedList *head = nullptr;
+    LinkedList *tail = nullptr;
 
-//     LinkedList *newHead = reverseLinkedList(head);
-//     print(newHead);
-// }
+    //  craete the linked list and add the nodes on the head;
+    // insertAtHead(head, tail, 10);
+    // print(head);
+    // insertAtHead(head, tail, 20);
+    // print(head);
+    // insertAtHead(head, tail, 30);
+    // print(head);
+    // insertAtHead(head, tail, 40);
+    // print(head);
+    // insertAtHead(head, tail, 50);
+    // print(head);
+
+    //  For reverse the Linked List.
+    // LinkedList *newHead = reverseLinkedList(head);
+    // print(newHead);
+
+    insertAtTail(head, tail, 10);
+    print(head);
+    insertAtTail(head, tail, 20);
+    print(head);
+    insertAtTail(head, tail, 30);
+    print(head);
+    insertAtTail(head, tail, 40);
+    print(head);
+    insertAtTail(head, tail, 50);
+    print(head);
+
+    int k = 4;
+    //  we need to left rotate the linked list k time..
+}
