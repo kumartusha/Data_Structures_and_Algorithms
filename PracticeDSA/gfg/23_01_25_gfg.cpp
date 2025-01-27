f6f80a5da2a9e03832dbcd3bfafe216a56f8dd5b
Tushar Kumar <kumartushar78867@gmail.com>
2025-01-27 23:31:53 +0530

    data-structures-and-algorithms

--- PracticeDSA/gfg/23_01_25_gfg.cpp
+++ PracticeDSA/gfg/23_01_25_gfg.cpp
@@ -65,149 +65,173 @@ using namespace std;
 //     // }
 // }
 
+// int main()
+// {
+// we need to find the computers that easily communicate to the other computer if it lies on a same row or column.
+// int count = 0;
+// for (int i = 0; i < row; ++i)
+// {
+//     for (int j = 0; j < col; ++j)
+//     {
+//         if (matrix[i][j])
+//         {
+//         }
+//     }
+//     // break;
+// }
+
+// cout << count;
+
+// for (auto element : matrix)
+// {
+//     for (auto element2 : element)
+//     {
+//         cout << element2 << " ";
+//     }
+//     cout << endl;
+// }
+
+// vector<vector<int>> matrix = {{1, 0, 1}, {0, 1, 1}, {0, 1, 0}, {1, 0, 0}};
+// int row = matrix.size();
+// int col = matrix[0].size();
+
+// vector<int> rowStore(row, 0);
+// vector<int> colStore(col, 0);
+
+// for (int i = 0; i < row; ++i)
+// {
+//     for (int j = 0; j < col; ++j)
+//     {
+//         if (matrix[i][j])
+//         {
+//             rowStore[i]++;
+//             colStore[j]++;
+//         }
+//     }
+// }
+
+// int count = 0;
+
+// for (int i = 0; i < row; ++i)
+// {
+//     for (int j = 0; j < col; ++j)
+//     {
+//         if (matrix[i][j])
+//         {
+//             if (rowStore[i] > 1 || colStore[j] > 1)
+//             {
+//                 ++count;
+//             }
+//         }
+//     }
+// }
+
+// cout << count;
+// unordered_map<Node *, bool> isVisited;
+
+// //  we need to traverse the linked list and when we got the visited node twice then we simply says that loop is present.
+// Node *temp = head;
+
+// while (temp != NULL)
+// {
+//     if (isVisited[temp] == true)
+//     {
+//         //  means that if the node is visited twice times.
+//         return true;
+//     }
+//     isVisited[temp] = true;
+//     temp = temp->next;
+// }
+// return false;
+
+//  we have given the integr into the inout we need to find the Character string.
+// int number = 5;
+
+// for (int i = 0; i < number; ++i)
+// {
+//     for (int j = 0; j < number; ++j)
+//     {
+//         if (i >= j)
+//         {
+//             cout << " * ";
+//         }
+//         else
+//         {
+//             cout << " ";
+//         }
+//     }
+//     cout << endl;
+// }
+
+// gfg 26 POTD
+// Node *temp = head;
+// unordered_map<Node *, bool> myMap;
+
+// while (temp->next != NULL)
+// {
+//     if (myMap[temp->next] == true)
+//     {
+//         //  loop is found.
+//         temp->next = NULL;
+//         break;
+//     }
+//     myMap[temp] = true;
+//     temp = temp->next;
+// }
+
+// int a = 5;
+// int b = 7;
+// int c = 4;
+// int d = 9;
+
+//     int a = 1;
+//     int b = 1500;
+//     int c = 2;
+//     int d = 500;
+
+//     // cout << a / b + c / d;
+//     int deno = -1;
+//     int denoMax = -1;
+//     int denoMin = -1;
+//     int nume = -1;
+//     //  make the denominator..
+//     if (b % d == 0 || d % b == 0)
+//     {
+//         // we have the same denominator number.
+//         deno = max(b, d);
+//         denoMin = min(b, d);
+//         nume = (denoMax / denoMin) + max(a, c);
+//     }
+//     else
+//     {
+//         deno = b * d;
+//         nume = a * d + b * c;
+//     }
+
+//     cout << deno;
+//     cout << nume;
+// }
+
 int main()
 {
-    // we need to find the computers that easily communicate to the other computer if it lies on a same row or column.
-    // int count = 0;
-    // for (int i = 0; i < row; ++i)
-    // {
-    //     for (int j = 0; j < col; ++j)
-    //     {
-    //         if (matrix[i][j])
-    //         {
-    //         }
-    //     }
-    //     // break;
-    // }
+    vector<int> arr = {1, 2, 3, 1, 2};
+    map<int, int> myMap;
 
-    // cout << count;
-
-    // for (auto element : matrix)
-    // {
-    //     for (auto element2 : element)
-    //     {
-    //         cout << element2 << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-    // vector<vector<int>> matrix = {{1, 0, 1}, {0, 1, 1}, {0, 1, 0}, {1, 0, 0}};
-    // int row = matrix.size();
-    // int col = matrix[0].size();
-
-    // vector<int> rowStore(row, 0);
-    // vector<int> colStore(col, 0);
-
-    // for (int i = 0; i < row; ++i)
-    // {
-    //     for (int j = 0; j < col; ++j)
-    //     {
-    //         if (matrix[i][j])
-    //         {
-    //             rowStore[i]++;
-    //             colStore[j]++;
-    //         }
-    //     }
-    // }
-
-    // int count = 0;
-
-    // for (int i = 0; i < row; ++i)
-    // {
-    //     for (int j = 0; j < col; ++j)
-    //     {
-    //         if (matrix[i][j])
-    //         {
-    //             if (rowStore[i] > 1 || colStore[j] > 1)
-    //             {
-    //                 ++count;
-    //             }
-    //         }
-    //     }
-    // }
-
-    // cout << count;
-    // unordered_map<Node *, bool> isVisited;
-
-    // //  we need to traverse the linked list and when we got the visited node twice then we simply says that loop is present.
-    // Node *temp = head;
-
-    // while (temp != NULL)
-    // {
-    //     if (isVisited[temp] == true)
-    //     {
-    //         //  means that if the node is visited twice times.
-    //         return true;
-    //     }
-    //     isVisited[temp] = true;
-    //     temp = temp->next;
-    // }
-    // return false;
-
-    //  we have given the integr into the inout we need to find the Character string.
-    // int number = 5;
-
-    // for (int i = 0; i < number; ++i)
-    // {
-    //     for (int j = 0; j < number; ++j)
-    //     {
-    //         if (i >= j)
-    //         {
-    //             cout << " * ";
-    //         }
-    //         else
-    //         {
-    //             cout << " ";
-    //         }
-    //     }
-    //     cout << endl;
-    // }
-
-    // gfg 26 POTD
-    // Node *temp = head;
-    // unordered_map<Node *, bool> myMap;
-
-    // while (temp->next != NULL)
-    // {
-    //     if (myMap[temp->next] == true)
-    //     {
-    //         //  loop is found.
-    //         temp->next = NULL;
-    //         break;
-    //     }
-    //     myMap[temp] = true;
-    //     temp = temp->next;
-    // }
-
-    // int a = 5;
-    // int b = 7;
-    // int c = 4;
-    // int d = 9;
-
-    int a = 1;
-    int b = 1500;
-    int c = 2;
-    int d = 500;
-
-    // cout << a / b + c / d;
-    int deno = -1;
-    int denoMax = -1;
-    int denoMin = -1;
-    int nume = -1;
-    //  make the denominator..
-    if (b % d == 0 || d % b == 0)
+    for (int i = 0; i < arr.size(); ++i)
     {
-        // we have the same denominator number.
-        deno = max(b, d);
-        denoMin = min(b, d);
-        nume = (denoMax / denoMin) + max(a, c);
-    }
-    else
-    {
-        deno = b * d;
-        nume = a * d + b * c;
+        myMap[arr[i]]++;
     }
 
-    cout << deno;
-    cout << nume;
+    int maxElement = INT_MIN;
+    int ans = -1;
+    for (auto element : myMap)
+    {
+        if (maxElement < element.second)
+        {
+            maxElement = element.second;
+            ans = element.first;
+        }
+        // cout << element.first << " " << element.second << endl;
+    }
+    cout << ans;
 }
