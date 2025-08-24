f71e13a7f50d6ea8c880d6526f3ca69b1d3e4fc9
Tushar Kumar <kumartushar78867@gmail.com>
2025-08-24 16:03:12 +0530

    Code and Debug

---
+++ Code_and_Debug/Day_002/first.py
@@ -0,0 +1,12 @@
+# def factorial(num):
+#     if num == 0 or num == 1:
+#         return 1
+    
+#     return num * factorial(num - 1)
+
+# if __name__ == "__main__":
+#     num = eval(input("Enter the number"))
+#     print("The Factorial are:-", factorial(num))
+
+# # This code calculates the factorial of a given number using recursion.
+
