3b38189bdd22c8eef9edff1d0888bd93f9225b3f
Tushar Kumar <kumartushar78867@gmail.com>
2025-08-25 10:21:20 +0530

    Code and Debug

--- Code_and_Debug/Day_002/first.py
+++ Code_and_Debug/Day_002/first.py
@@ -10,3 +10,110 @@
 
 # # This code calculates the factorial of a given number using recursion.
 
+## Reverse an array using the recursion
+
+
+# def reverse(array, start, end):
+#     if start == end:
+#         return
+    
+#     # else we need to swap the elements of first and last.
+#     array[start],array[end] = array[end],array[start]
+
+#     reverse(array, start + 1, end - 1)
+
+
+# if __name__ == "__main__":
+#     array = [5,1,2,4,3]
+#     start = 0
+#     end = len(array) - 1
+#     reverse(array, start, end)
+
+#     print(array)
+
+# def isPalindrome(myStr, start, end):
+#     if start >= end:
+#         return True
+    
+#     # check if the last and the first character is same or not.
+#     if myStr[start] != myStr[end]:
+#         return False
+    
+#     return isPalindrome(myStr, start + 1, end - 1)
+#     #  we need to 
+
+
+# if __name__ == "__main__":
+#     myStr = "madam"
+
+#     #  we need to tell this is a palindrome string or not ??
+#     if isPalindrome(myStr, 0, len(myStr) - 1):
+#         print("The string is palindrome")
+#     else:
+#         print("Not the palindrome")
+
+
+
+# if __name__ == "__main__":
+#     myStr = "nitin"
+#     start = 0
+#     end = len(myStr) - 1
+
+#     for i in myStr:
+#         if myStr[start] != myStr[end]:
+#             print("The string is not palindrome")
+#             break
+#         start += 1
+#         end -= 1
+#     else:
+#         print("The string is palindrome")
+# def isFibonacci(user):
+#     if user < 1:
+#         print("Enter number greater than or equal to 1")
+#     if user == 1:
+#         return 0 
+#     if user <= 3:
+#         return 1
+    
+#     return isFibonacci(user - 1) + isFibonacci(user- 2)
+
+
+# #  find the fibonacci number using the recursion.
+# if __name__ == "__main__":
+#     user = int(input("Enter the number of fibonacci"))
+
+#     print(isFibonacci(user))
+
+
+#.  Sorting starts from Here.
+
+# we will be discussed about the Bubble Sorting.
+
+# def bubbleSort(array):
+#     for first in range(len(array)):
+#         for second in range(len(array) - first):
+#             if array[second] > array[second + 1]:
+#                 array[second],array[second+1] = array[second+1],array[second] 
+# if __name__ == "__main__":
+#     # here we will be implemented the bubble sort.
+#     array = [5,4,3,2,1]
+
+#     bubbleSort(array)
+
+#     print(array)
+
+# def selectionSort(array):
+#     for i in range(len(array)):
+#         minIndex = i
+#         for j in range(i+1, len(array)):
+#             if array[minIndex] > array[j]:
+#                 minIndex = j
+#         # swap the elements.
+#         array[minIndex], array[i] = array[i],array[minIndex]
+
+# if __name__ == "__main__":
+#     array = [5,4,3,2,1]
+#     selectionSort(array)
+
+#     print(array)
+
