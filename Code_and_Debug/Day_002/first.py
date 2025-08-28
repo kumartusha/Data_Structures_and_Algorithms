3fa4b2f1e083d6a5524df87d25f5f5b1cb471f73
Tushar Kumar <kumartushar78867@gmail.com>
2025-08-28 09:12:22 +0530

    Sorting ALgorithms

--- Code_and_Debug/Day_002/first.py
+++ Code_and_Debug/Day_002/first.py
@@ -126,3 +126,79 @@
 #     array = [5,4,3,2,1]
 
 #     insertionSort(array)
+
+# def selectionSort(array):
+#     for i in range(len(array)):
+#         minIndex = i
+#         for j in range(i + 1, len(array)):
+#             if array[j] > array[minIndex]:
+#                 minIndex = j
+#         # Swap the elements.
+#         array[i], array[minIndex] = array[minIndex], array[i]
+
+
+# if __name__ == "__main__":
+#     array = [7,5,6,8,4,7,5,6,4,3,7,5,6,9]
+
+#     print("Before Sorting:- ", array)
+
+#     selectionSort(array)
+
+#     print("After Sorting:- ", array)
+
+# def bubbleSort(array):
+#     for i in range(len(array)):
+#         for j in range(len(array) - i):
+#             if array[j] > array[j + 1]:
+#                 array[j], array[j+1] = array[j+1], array[j]
+
+# def bubbleSort(array):
+
+#     for first in range(len(array)):
+#         for second in range((len(array) -1) - first):
+#             if array[second] > array[second + 1]:
+#                 array[second], array[second + 1] = array[second + 1], array[second]
+
+# if __name__ == "__main__":
+#     array = [9,1,4,8,5,6,2,8,10,3,33]
+
+#     print("Before Sorting:- ", array)
+
+#     bubbleSort(array)
+
+#     print("After Sorting:- ", array)
+
+# def insertionSort(array):
+#     for i in range(1, len(array)):
+#         currentElement = array[i]
+#         j = i - 1
+#         while(j >= 0 and currentElement < array[j]):
+#             array[j + 1] = array[j]
+#             j = j - 1
+
+#         array[j + 1] = currentElement
+
+# def merge(array, start, end):
+     # Here we need to implement the merge function that will be merge the two sub recursive arrays.
+
+
+def mergeSort(array, start, end):
+     if start >= end:
+          return
+     
+     # find the mid and call the merge sort for the left right nd then merge Function.
+
+     mid = start + (end - start) / 2
+
+     mergeSort(array, start, mid)
+     mergeSort(array, mid + 1, end)
+    #  merge(array, start, end)
+
+if __name__ == "__main__":
+    array = [6,5,4,3,2,1]
+
+    print("Before Sorting:- ", array)
+
+    mergeSort(array, 0, len(array) - 1)
+
+    print("After Sorting:- ", array)
